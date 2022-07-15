ebdb6cedeefdddb8c22950dac4a9fd41bd2d1e9a
HaoCheng <ch1994@outlook.com>
2022-07-15 10:50:07 +0800

    update ci

--- Bucket.Tests.ps1
+++
@@ -1,5 +0,0 @@
-if (-not ($env:SCOOP_HOME)) {
-    $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop)
-}
-
-. "$env:SCOOP_HOME\test\Import-Bucket-Tests.ps1"
--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "ScoopInstaller/Main:master"
+    [String]$upstream = "tkit1994/scoop_bucket"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
